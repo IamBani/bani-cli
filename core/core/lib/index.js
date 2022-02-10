bbce2dac5d0203ff40fae6136268eb34a69dd128
IamBani <13972186114@139.com>
2022-02-10 17:35:41 +0800

    node判断版本

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,7 +1,23 @@
 'use strict';
 
+const colors = require('colors')
+const semver = require('semver')
+const log = require("@bani-cli/log");
+const pkg = require("../package.json")
+const { LOWEST_NODE_VERSION } = require("./const");
+
 module.exports = core;
 
 function core() {
     // TODO
+  checkPkgVersion()
+  checkNodeVersion()
+}
+
+function checkNodeVersion () {
+  console.log(process.version>LOWEST_NODE_VERSION)
+}
+
+function checkPkgVersion () {
+  log(pkg.version)
 }
---
+++ utils/log/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const npmlog = require("npmlog")
+
+module.exports = index;
+
+
+function index(pkg) {
+    // TODO
+  npmlog.info('cli',pkg)
+}
