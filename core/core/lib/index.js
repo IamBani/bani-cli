9764433c721be9e8c70ade3e069cf40426df6c4b
IamBani <13972186114@139.com>
2022-08-31 17:41:02 +0800

    feat(core/core/lib): root 权限 用户主目录 --debug 参数

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,24 +1,64 @@
 'use strict';
+const os = require('os')
+const path = require('path')
 
 const chalk = require('chalk')
 const semver = require('semver')
 const {log,npmlog} = require("@bani-cli/log");
 const pkg = require("../package.json")
-const { LOWEST_NODE_VERSION} = require("./const");
+const { LOWEST_NODE_VERSION} = require("./const")
 const { sum } = require("@bani-cli/utils")
-const {commands} =require("@bani-cli/commands")
+const { commands } = require("@bani-cli/commands")
+const rootCheck = require('root-check')
+const dotenv = require('dotenv')
+const minimist = require('minimist')
 module.exports = core;
 
 function core() {
     // TODO
-  checkPkgVersion()
-  checkNodeVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+    checkRoot()
+    checkUserHome()
+    checkInputArgs()
+    checkEnv()
+  } catch (e) {
+    npmlog.error(e.message)
+  }
+}
+
+function checkEnv () {
+  
+}
+
+
+function checkInputArgs () {
+  const args = minimist(process.argv.slice(2))
+  console.log(args);
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose'
+  } else {
+    process.env.LOG_LEVEL = 'info'
+  }
+  npmlog.level = process.env.LOG_LEVEL
+  npmlog.verbose('de1','test')
+}
+
+
+function checkUserHome () {
+  if (!os.homedir || !os.homedir()) {
+    throw new Error(chalk.red('当前用户主目录不存在'))
+  }  
+}
+
+function checkRoot () {
+  rootCheck()
 }
 
 function checkNodeVersion () {
-  console.log(chalk.hex('#f40').bold('Hello')) 
   if (!semver.gte(process.version,LOWEST_NODE_VERSION)) {
-    console.log(chalk.hex('#DEADED').underline('Hello, world!'))  
+    throw new Error(chalk.red(`bani-cli需要${LOWEST_NODE_VERSION} 以上的node版本`))
   }
 }
 
