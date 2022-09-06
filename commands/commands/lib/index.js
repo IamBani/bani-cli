9168d15c4f0d755e9d2d6cf7be37750f4d3081d4
IamBani <13972186114@139.com>
2022-09-06 17:49:59 +0800

    feat: 新增commands 解析参数的命令

--- commands/commands/lib/index.js
+++ commands/commands/lib/index.js
@@ -1,7 +1,35 @@
 'use strict';
+const chalk = require('chalk')
+const { Command } = require('commander')
+
+const {npmlog} = require("@bani-cli/log");
+
+const program = new Command()
+
+function commands (pkg) {
+  program
+    .name(Object.keys(pkg['bin'])[0])
+    .usage('<commande> [options]')
+    .argument('<name>', '登录用户名')
+    .argument('[password]', '登录密码， 默认密码123456')
+    .argument('[other...]', '其他参数').version(pkg.version, '-v,--version')
+  program.command('create <app-name> [options]')
+    .description('create a new project powered by vue-cli-service')
+    .option('-f, --force','是否强制克隆')
+    .action(function(options,name) {
+      console.log('options', options, name);
+      console.log(this,arguments);
+    })
+  program.on('command:*', function (obj) {
+    const futrueCommand = program.commands.map(cmd => cmd.name())
+
+    npmlog.error(chalk.red(`未知的命令 ${obj.join()}`));
+    program.outputHelp()
+  })
+  if (program.args && program.args.length < 1) {
+    program.outputHelp()
+  }
+  program.parse(process.argv);
+}
 
 module.exports = { commands };
-
-function commands() {
-    return "Hello from commands";
-}
--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -29,6 +29,7 @@ function core() {
     checkInputArgs()
     checkEnv()
     checkGlobalUpdate()
+    commands(pkg)
   } catch (e) {
     npmlog.error(e.message)
   }
